c78d7ca6188f38f48d0d10c165c5a394f1a5de8b
Ashok Paudel <ashokpoudel2051@gmail.com>
2018-08-24 12:06:16 +0545

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController    #welcomecontroller is created write in camelcase
+
+	def home  #the action  home of controller welcomecontroller is created
+    end
+
+    def about
+    end
+
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+	get 'welcome/home', to: 'welcome#home'
+
+
+	#welcome/home will shown at the browser and action will be welcome controller ko home action dinxa
+	#now routes are defined and we have to define controller and action of this route so goto controller
+
+	get 'welcome/about', to: 'welcome#about'  
+	#when about will be called then welcome action ko about action ma janxa
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
